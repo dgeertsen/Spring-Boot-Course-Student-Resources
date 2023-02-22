ca91506f63970304711cbbb7cc9de63a590e5919
dgeertsen <devon.geertsen@gmail.coom>
2023-02-21 18:30:55 -0700

    Deterministic Testing with an In-memory Database

--- src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
+++ src/test/java/com/promineotech/jeep/controller/FetchJeepTest.java
@@ -3,6 +3,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.math.BigDecimal;
 import java.util.LinkedList;
 import java.util.List;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,9 +14,11 @@ import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlConfig;
+import org.springframework.test.jdbc.JdbcTestUtils;
 import com.promineotech.jeep.entity.Jeep;
 import com.promineotech.jeep.entity.JeepModel;
 
@@ -24,8 +27,8 @@ import com.promineotech.jeep.entity.JeepModel;
 @ActiveProfiles("test")
 //Load SQL Scripts that will be executed
 @Sql(scripts = {
-    "classpath:src/migrations/V1.0__Jeep_Schema.sql",
-"classpath:s/migrations/V1.1__Jeep_Data.sql"}, 
+    "classpath:flyway/migrations/V1.0__Jeep_Schema.sql",
+"classpath:flyway/migrations/V1.1__Jeep_Data.sql"}, 
 config = @SqlConfig(encoding = "utf-8"))
 
 class FetchJeepTest  {
@@ -37,6 +40,16 @@ class FetchJeepTest  {
   @LocalServerPort
   private int serverPort;
   
+//  @Autowired
+//  private JdbcTemplate jdbcTemplate;
+//  Test how many rows are in h2 db
+//  @Test 
+//  void testDb() {
+//    int numRows=JdbcTestUtils.countRowsInTable(jdbcTemplate, "customers");
+//    System.out.println("num="+numRows);
+//  }
+  
+
   @Test
   void testThatJeepsAreReturendWhenAValidJeepModelAndTrimAreSupplied() {
   //Given: a valid model, trim and URI 
